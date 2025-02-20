5cd6d87e508053d8491f944739d3ca514027edee
Nicole Su <tingchis@usc.edu>
2025-02-19 22:10:34 -0500

    Add

--- hw1-ts885/p1/generate.py
+++ hw1-ts885/p1/generate.py
@@ -1,22 +1,13 @@
 from hashlib import sha256
-import random
 import secrets
 
 
 netid = "ts885" # change to your netid
-testid = "af535"
-testforgedid = "qn00061"
+# testid = "af535"
+k = 4
+n = 28
 
 # Put your solution generation code here
-
-encoded_netid = testid.encode('ascii')
-print(encoded_netid)
-def hash_to_bin(s):
-    return bin(int(sha256(s).hexdigest(), base=16)).lstrip('0b').zfill(256)
-
-w = hash_to_bin(encoded_netid )[:16]
-print(w)
-
 
 def generate_watermark(id):
     encoded_id = id.encode('utf-8')
@@ -25,24 +16,35 @@ def generate_watermark(id):
 
     # print(id_hex_digest)
     watermark = id_hex_digest[:4]
-    print(watermark)
     return watermark
 
+watermark = generate_watermark(netid)
+print("wm: " + watermark)
 
-def find_forged(watermark):
-    seen_hashes = {}
-    max_attempts = 100000  # Limit the number of attempts to avoid infinite loops
+def find_coins(k, n, watermark): 
+    collide_map = {}
+    while True:
+        # Generate 6 random bytes
+        random_bytes = secrets.token_bytes(6)
+        random_hex = random_bytes.hex()
+        # Generate preimage, 8 bytes total
+        preimage = watermark + random_hex
+        hashed_preimage = sha256(preimage.encode('utf-8')).hexdigest()
 
-    for i in range(max_attempts):
-        # Generate a random string of characters
-        random_string1 = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k=20))
-        hash_value1 = sha256(random_string1.encode()).hexdigest()
-        truncated_hash1 = hash_value1[:4]  # Take only the necessary hex characters
-
-        if truncated_hash1 in seen_hashes:
-            random_string2 = seen_hashes[truncated_hash1]
-            return random_string2, random_string1
+        short_hashed_preimage = hashed_preimage[:n // 4]
         
-        seen_hashes[truncated_hash1] = random_string1
+        # add to map and count 
+        if short_hashed_preimage in collide_map:
+            collide_map[short_hashed_preimage].append(preimage)
+            if len(collide_map[short_hashed_preimage]) == k:
+                return collide_map[short_hashed_preimage]
+        else:
+            collide_map[short_hashed_preimage] = [preimage]
 
-    return None
+colliding_coins = find_coins(k, n, watermark)
+print("Colliding Coins:", colliding_coins)
+
+# write to coin.txt
+with open("coin.txt", "w") as f:
+    for coin in colliding_coins:
+        f.write(coin + "\n")
